1e3f213dc44797c797fdf8ab62a4685ad45be135
salvarezf <saaf@SAAF-MAC-PRO.local>
2018-10-18 23:59:10 +0200

    Added Route to components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 import PropTypes from 'prop-types';
+import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContact from './CreateContact';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   state = {
-    contacts : [
-      {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        "handle": "@karen_isgrigg",
-        "avatarURL": "http://localhost:5001/karen.jpg"
-      },
-      {
-        "id": "richard",
-        "name": "Richard Kalehoff",
-        "handle": "@richardkalehoff",
-        "avatarURL": "http://localhost:5001/richard.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "@tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
-  
+    contacts : []
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll()
+      .then((contacts) => {
+        this.setState(()=> ({
+        contacts
+      }))
+    })
   }
 
   removeContact  = (contact) => {
@@ -33,13 +25,23 @@ class App extends Component {
         return c.id !== contact.id
       })
     }))
+
+    ContactsAPI.removeContact(contact);
   }
   render() {
     return (
       <div>
+      <Route exact path='/' render={() => (
         <ListContacts 
         contacts={this.state.contacts}
-        onDeleteContact={this.removeContact}/>
+        onDeleteContact={this.removeContact}
+        />
+        
+
+      )}/>
+     
+        <Route path='/create' component={CreateContact}
+        />
       </div>
     );
   }
